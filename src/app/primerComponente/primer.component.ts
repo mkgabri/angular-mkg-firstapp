8c30f6af29b3c1ab4b0927e2e2502b414eeec3ba
mkgabri <gabriel136@hotmail.it>
2019-10-03 15:31:31 +0200

    primera pagina definitiva con angular

--- src/app/primerComponente/primer.component.ts
+++ src/app/primerComponente/primer.component.ts
@@ -2,14 +2,10 @@ import { Component } from '@angular/core';
 
 @Component ({
   selector: 'hello-world',
-  template: `
-    <div class="hello-world">
-      <h1>Hola mi mundo</h1>
-      <h2> todo bien </h2>
-    </div>
-  `,
-  styles: ['.hello-world { background: #000; color: #fff;}']
+  templateUrl: './primer.component.html',
+  styleUrls: ['./primer.component.css']
 })
 export class HelloWorld {
-
+  title = 'Bienvenido';
+  subtitle = 'Mi primera pagina en angular.'
 }
