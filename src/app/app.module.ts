55272ddf6fc08474a656a3155a8e2577baee74d6
mkgabri <gabriel136@hotmail.it>
2019-10-04 23:01:44 +0200

    add segundo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HelloWorld } from './primerComponente/primer.component';
+import { ListVector } from './segundoComponente/segundo.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HelloWorld ],
+  declarations: [ AppComponent, HelloComponent, HelloWorld, ListVector ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/segundoComponente/segundo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component ({
+  selector: 'list-vector',
+  templateUrl: './segundo.component.html',
+  styleUrls: ['./segundo.component.css']
+})
+export class ListVector {
+  users = ['HTML5', 'CSS3', 'JS', 'ANGULAR'];
+}
